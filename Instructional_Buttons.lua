ae4e4d26c83e6667cd3aa90f22ddde4ada366884
AntwanR942 <46630572+AntwanR942@users.noreply.github.com>
2019-09-15 20:53:03 +0100

    Add files via upload

---
+++ Instructional_Buttons.lua
@@ -0,0 +1,75 @@
+function ButtonMessage(text)
+    BeginTextCommandScaleformString("STRING")
+    AddTextComponentScaleform(text)
+    EndTextCommandScaleformString()
+end
+
+function Button(ControlButton)
+    N_0xe83a3e3557a56640(ControlButton)
+end
+
+function setupScaleform(scaleform)
+
+    local scaleform = RequestScaleformMovie(scaleform)
+
+    while not HasScaleformMovieLoaded(scaleform) do
+        Citizen.Wait(1)
+    end
+
+    PushScaleformMovieFunction(scaleform, "CLEAR_ALL")
+    PopScaleformMovieFunctionVoid()
+    
+    PushScaleformMovieFunction(scaleform, "SET_CLEAR_SPACE")
+    PushScaleformMovieFunctionParameterInt(200)
+    PopScaleformMovieFunctionVoid()
+
+    PushScaleformMovieFunction(scaleform, "SET_DATA_SLOT")
+    PushScaleformMovieFunctionParameterInt(5)
+    Button(GetControlInstructionalButton(2, config.controls.openKey, true))
+    ButtonMessage("Disable Noclip")
+    PopScaleformMovieFunctionVoid()
+
+    PushScaleformMovieFunction(scaleform, "SET_DATA_SLOT")
+    PushScaleformMovieFunctionParameterInt(4)
+    Button(GetControlInstructionalButton(2, config.controls.goUp, true))
+    ButtonMessage("Go Up")
+    PopScaleformMovieFunctionVoid()
+
+    PushScaleformMovieFunction(scaleform, "SET_DATA_SLOT")
+    PushScaleformMovieFunctionParameterInt(3)
+    Button(GetControlInstructionalButton(2, config.controls.goDown, true))
+    ButtonMessage("Go Down")
+    PopScaleformMovieFunctionVoid()
+
+    PushScaleformMovieFunction(scaleform, "SET_DATA_SLOT")
+    PushScaleformMovieFunctionParameterInt(2)
+    Button(GetControlInstructionalButton(1, config.controls.turnRight, true))
+    Button(GetControlInstructionalButton(1, config.controls.turnLeft, true))
+    ButtonMessage("Turn Left/Right")
+    PopScaleformMovieFunctionVoid()
+
+    PushScaleformMovieFunction(scaleform, "SET_DATA_SLOT")
+    PushScaleformMovieFunctionParameterInt(1)
+    Button(GetControlInstructionalButton(1, config.controls.goBackward, true))
+    Button(GetControlInstructionalButton(1, config.controls.goForward, true))
+    ButtonMessage("Go Forwards/Backwards")
+    PopScaleformMovieFunctionVoid()
+
+    PushScaleformMovieFunction(scaleform, "SET_DATA_SLOT")
+    PushScaleformMovieFunctionParameterInt(0)
+    Button(GetControlInstructionalButton(2, config.controls.changeSpeed, true))
+    ButtonMessage("Change Speed ("..config.speeds[index].label..")")
+    PopScaleformMovieFunctionVoid()
+
+    PushScaleformMovieFunction(scaleform, "DRAW_INSTRUCTIONAL_BUTTONS")
+    PopScaleformMovieFunctionVoid()
+
+    PushScaleformMovieFunction(scaleform, "SET_BACKGROUND_COLOUR")
+    PushScaleformMovieFunctionParameterInt(config.bgR)
+    PushScaleformMovieFunctionParameterInt(config.bgG)
+    PushScaleformMovieFunctionParameterInt(config.bgB)
+    PushScaleformMovieFunctionParameterInt(config.bgA)
+    PopScaleformMovieFunctionVoid()
+
+    return scaleform
+end
